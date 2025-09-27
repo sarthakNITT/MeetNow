017eec81d4a367f8a8bb464798f6e6dabb209aeb
sarthakKarode <sarthakkarode@gmail.com>
2025-09-27 10:43:32 +0530

    Client → Server: join

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,66 +1,86 @@
-const rooms = new Map<string, Set<string>>();
-const peers = new Map<string, {socket: any, roomId: string, name: string}>();
+interface roomInterface {
+    roomId: string,
+    peers: {
+        peerSocket: any,
+        peerId: string,
+        peerName: string
+    }[]
+}
 
 let checkJoinReq = false;
-const server = Bun.serve({
+let rooms: roomInterface[] = [];
+
+Bun.serve({
     port: 8080,
     fetch(req, server) {
-        const success = server.upgrade(req);
-        if(success) {
-            return undefined;
+        if (server.upgrade(req)) {
+            return;
         }
-        return new Response("hello world")
+        return new Response("Upgrade failed", { status: 500 });
     },
     websocket: {
-        open(ws){
-            let result = '';
-            const ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-            const length = 5;
+        open (ws) {
+            let socketId = '';
+            const characters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()';
+            const length = 8;
             for(let i=0;i<length;i++){
-                result += ch.charAt(Math.floor(Math.random() * ch.length));
+                socketId += characters.charAt(Math.floor(Math.random() * characters.length));
             }
-            const socketId = result;
-            ws.send(socketId);
+            console.log(`SocketId: ${socketId}`);
+            ws.send(JSON.stringify({
+                type: "connection",
+                value: `${socketId}`
+            }))
             setTimeout(() => {
                 if(!checkJoinReq){
+                    console.log(`No joining request for 10 sec, Closing the Websocket`);
                     ws.close();
-                    console.log("ws closed");
-                    
                     return;
                 }
             }, 10000);
-        },
-        message(ws, message) {
-            const recievedMessage = JSON.stringify(message);
-            if(!recievedMessage.includes("join")) {
-                ws.close();
-                console.log("ws closed");
-            }
-            checkJoinReq = true;
-            ws.send(`join request recieved`);
-            if(!recievedMessage.includes("roomId") || !recievedMessage.includes("peerId") || !recievedMessage.includes("name")){
-                ws.send(`{ "type": "error", "message": "Invalid join message" }`)
-                ws.close();
-                console.log("ws closed");
-                return;
-            }
-            const msg = JSON.parse(recievedMessage);
-            if(!rooms.has(msg.roomId)){
-                rooms.set(msg.roomId, new Set());
-                console.log("new room created");
-            }else{
-                console.log("Room already exists");
-            }
-            rooms.get(msg.roomId)?.add(msg.peerId);
-            peers.set(msg.peerId, {socket: ws, roomId: msg.roomId, name: msg.name});
-            ws.send(`{ type: "joined", peers: ${peers}}`);
-            peers.forEach((e)=>{
-                if(e.roomId === msg.roomId){
-                    ws.send(`{ type: "new-peer", peer: { ${msg.peerId}, ${msg.name} } }`);
+        }, message (ws, message: string) {
+            const msg = JSON.parse(message);
+            if(msg.type === "join"){
+                checkJoinReq = true;
+                console.log(`Join request recieved`);
+                if(!msg.roomId || !msg.peerId || !msg.name){
+                    console.log(`Incorrect message type`);
+                    return;
                 }
-            })
+                const checkExistingRoom = rooms.filter((e)=>e.roomId === msg.roomId);
+                console.log(`rooms: ${checkExistingRoom}`);
+                if(checkExistingRoom.length === 0){
+                    console.log(`Creating room`);
+                    rooms.push({
+                        roomId: msg.roomId,
+                        peers: [{
+                            peerSocket: ws,
+                            peerId: msg.peerId,
+                            peerName: msg.name
+                        }]
+                    })
+                }else{
+                    console.log(`Room already exists, adding peer`);
+                    const existingRoom = rooms.find(room => room.roomId === msg.roomId);
+                    if(existingRoom){
+                        existingRoom.peers.push({
+                            peerSocket: ws,
+                            peerId: msg.peerId,
+                            peerName: msg.name
+                        });
+                        existingRoom.peers.forEach((peer)=>{
+                            peer.peerSocket.send(JSON.stringify({
+                                type: "new-peer",
+                                value: `peerId: ${msg.peerId}, peerName: ${msg.name}`
+                            }))
+                        })
+                    }
+                }
+                ws.send(JSON.stringify({
+                    type: "JoinRequest",
+                    value: "Joined room"
+                }))
+            }
         }
     }
 })
-
-console.log(`listening on ${server.hostname}:${server.port}`);
